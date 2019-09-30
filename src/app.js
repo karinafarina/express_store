b03589c27af3e940d79cb3b3c587ccc75802dde7
Karina Gaulin <karina@domandtom.com>
2019-09-30 14:31:28 -0400

    Add post and delete

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const uuid = require('uuid/v4');
 
 const app = express()
 
@@ -16,15 +17,27 @@ app.use(express.json());
 app.use(helmet())
 app.use(cors())
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res
-    .send('POST request received.')
-});
+const users = [
+  {
+    "id": "3c8da4d5-1597-46e7-baa1-e402aed70d80",
+    "username": "sallyStudent",
+    "password": "c00d1ng1sc00l",
+    "favoriteClub": "Cache Valley Stone Society",
+    "newsLetter": "true"
+  },
+  {
+    "id": "ce20079c-2326-4f17-8ac4-f617bfd28b7f",
+    "username": "johnBlocton",
+    "password": "veryg00dpassw0rd",
+    "favoriteClub": "Salt City Curling Club",
+    "newsLetter": "false"
+  }
+];
 
 app.post('/user', (req, res) => {
   //get the data
   const { username, password, favoriteClub, newsLetter=false } = req.body;
+  console.log(username);
 
   //validation code here
   if (!username) {
@@ -71,13 +84,40 @@ app.post('/user', (req, res) => {
       .stutus(400)
       .send('Not a valid club');
   }
+  const id = uuid();
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter
+  };
+  users.push(newUser);
   //at this point all the validation passed
-  res.send('All validation passed');
+  res
+    .status(201)
+    .location(`http://localhost:8000/user/${id}`)
+    .json({ id: id });
 })
 
-app.get('/', (req, res) => {
+app.delete('/user/:userId', (req,res) => {
+  const { userId } = req.params;
+ 
+  const index = users.findIndex(u => u.id === userId);
+
+  //make sure we actually find a user with that id
+  if(index === -1) {
+    return res
+      .status(404)
+      .send('User not found');
+  }
+  users.splice(index, 1);
+  res.send('Deleted.');
+});
+
+app.get('/user', (req, res) => {
   res
-    .send('A GET Request');
+    .json(users);
 })
 
 app.use(function errorHandler(error, req, res, next) {
